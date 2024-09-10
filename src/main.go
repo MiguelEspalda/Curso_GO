7a4c93995fdf869865857d67a5f89c70fcc1aab3
MiguelEspalda <martinezcobosm@gmail.com>
2024-09-10 14:47:45 -0500

    Variables - Golang

--- src/main.go
+++ src/main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Prueba de codigo en GO")
+
+	//Declaracion de una Costante
+	const num float64 = 3.14
+	const num2 = 3.1415
+
+	fmt.Println("Numero 1:", num)
+	fmt.Println("Numero 2:", num2)
+
+	//Declaracion de variables enteras --> Si no estan siendo usadas saltara un error
+	num3 := 5
+	var num4 = 6
+	var num5 int
+
+	fmt.Println(num3, num4, num5)
+
+	//Zero values
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	//Variables Constantes
+	const baseCuadrada = 10
+	areaCuadrada := baseCuadrada * baseCuadrada
+	fmt.Println("Area Cuadrada: ", areaCuadrada)
 }
